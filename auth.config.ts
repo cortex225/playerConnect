331dd8fbb8da16864cacffe6aeb9ca9f352a04a0
jlgouaho <1900751@CegepGranby.qc.ca>
2024-12-05 00:28:03 -0500

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -14,11 +14,7 @@ export default {
     Google({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          redirect_uri: 'https://player-connect.vercel.app/api/auth/callback/google',
-        },
-      },
+    
     }),
     Resend({
       apiKey: env.RESEND_API_KEY,
