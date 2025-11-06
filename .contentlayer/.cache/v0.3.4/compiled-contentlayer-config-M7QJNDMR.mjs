14799b1ec31289e33b92a7222c69b2aae9d621a2
cortex225 <jlgouaho@gmail.com>
2025-11-06 15:16:52 -0500

    new

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-M7QJNDMR.mjs
@@ -0,0 +1,67 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `blog/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    date: { type: "date", required: true },
+    published: { type: "boolean", default: true },
+    image: { type: "string", required: false },
+    authors: { type: "list", of: { type: "string" }, required: false },
+    category: { type: "string", required: false }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "")
+    }
+  }
+}));
+var Guide = defineDocumentType(() => ({
+  name: "Guide",
+  filePathPattern: `guides/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    date: { type: "date", required: true },
+    published: { type: "boolean", default: true },
+    featured: { type: "boolean", default: false }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "")
+    }
+  }
+}));
+var Doc = defineDocumentType(() => ({
+  name: "Doc",
+  filePathPattern: `docs/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    published: { type: "boolean", default: true }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "")
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content",
+  documentTypes: [Post, Guide, Doc]
+});
+export {
+  Doc,
+  Guide,
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-M7QJNDMR.mjs.map
---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-ZMJJXGR4.mjs
@@ -0,0 +1,97 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `blog/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    date: { type: "date", required: true },
+    published: { type: "boolean", default: true },
+    image: { type: "string", required: false },
+    authors: { type: "list", of: { type: "string" }, required: false },
+    categories: { type: "list", of: { type: "string" }, required: false },
+    related: { type: "list", of: { type: "string" }, required: false },
+    category: { type: "string", required: false }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "")
+    }
+  }
+}));
+var Guide = defineDocumentType(() => ({
+  name: "Guide",
+  filePathPattern: `guides/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    date: { type: "date", required: true },
+    published: { type: "boolean", default: true },
+    featured: { type: "boolean", default: false }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "")
+    },
+    slugAsParams: {
+      type: "string",
+      resolve: (doc) => doc._raw.flattenedPath.replace(/^guides\//, "")
+    }
+  }
+}));
+var Doc = defineDocumentType(() => ({
+  name: "Doc",
+  filePathPattern: `docs/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    published: { type: "boolean", default: true }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "")
+    },
+    slugAsParams: {
+      type: "string",
+      resolve: (doc) => doc._raw.flattenedPath.replace(/^docs\//, "")
+    }
+  }
+}));
+var Page = defineDocumentType(() => ({
+  name: "Page",
+  filePathPattern: "pages/**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "")
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content",
+  documentTypes: [Post, Guide, Doc, Page],
+  mdx: {
+    remarkPlugins: [],
+    rehypePlugins: []
+  }
+});
+export {
+  Doc,
+  Guide,
+  Page,
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-ZMJJXGR4.mjs.map
